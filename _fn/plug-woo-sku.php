b133428de8e0062a5645f6f79e0481e97251e214
Jonathan Moore <Jonathan.Moore@bcs.org>
2018-08-19 20:42:02 +0100

    sku enhancements

--- _fn/plug-woo-sku.php
+++ _fn/plug-woo-sku.php
@@ -34,6 +34,7 @@ if ( skuformat() ) {
 	if ( is_ajax() ) {
 		// generate SKUs when variations are saved via ajax
 		add_action( 'woocommerce_ajax_save_product_variations', 'ajax_maybe_generate_variation_skus', 10, 1 );
+		add_action( 'woocommerce_save_product_variation', 'ink_maybe_generate_variation_sku', 10, 2 );
 	} elseif ( is_admin() ) {
 
 		//set the sku after first save of new product if still blank
@@ -44,15 +45,16 @@ if ( skuformat() ) {
 		// save the generated SKUs during product edit / bulk edit
 		add_action( 'woocommerce_product_bulk_edit_save', 'ink_maybe_generate_and_save_sku', 10, 1 );
 		add_action( 'woocommerce_process_product_meta', 'ink_maybe_generate_and_save_sku', 100, 2 );
+
 		//add_action( 'woocommerce_new_product', 'ink_maybe_generate_and_save_sku', 100, 1 );
 		//add_action( 'woocommerce_update_product', 'ink_maybe_generate_and_save_sku', 100, 1 );
 
-		add_action( 'woocommerce_save_product_variation', 'maybe_save_variation_sku', 10, 2 );
+		add_action( 'woocommerce_save_product_variation', 'ink_maybe_generate_variation_sku', 10, 2 );
 
+		add_action( 'woocommerce_product_duplicate_before_save', 'ink_product_duplicate_before_save', 10, 2 );
 
-		add_filter( 'woocommerce_duplicate_product_exclude_meta', 'ink_duplicate_product_exclude_meta', 10, 1 );
-//woocommerce_product_variation_title
-//add_action( 'woocommerce_duplicate_product', 'set_new_product_sku_on_duplicate',   PHP_INT_MAX, 2 );
+		//add_filter( 'woocommerce_duplicate_product_exclude_meta', 'ink_duplicate_product_exclude_meta', 10, 1 );
+		add_action( 'woocommerce_duplicate_product', 'ink_new_product_sku_on_duplicate', PHP_INT_MAX, 2 );
 	}
 }
 function post_is_translation( $post ) {
@@ -74,12 +76,30 @@ function post_is_translation( $post ) {
 }
 
 /*
- * add sku to the list of attributes to be ignored when duplicating..
+ * WooCommerce product_duplicate adds a "-n" suffix to sku of duplicated product
+ * here we remove it so it can be generated using the rules here
  */
-function ink_duplicate_product_exclude_meta( $excludes ) {
-	$excludes[] = '_sku';
-	return $excludes;
+function ink_product_duplicate_before_save( $duplicate, $product ) {
+	$duplicate->set_sku( '' );
 }
+
+/*
+ * after duplicating product, set the sku
+ */
+function ink_new_product_sku_on_duplicate( $duplicate, $product ) {
+	ink_maybe_generate_and_save_sku( $duplicate );
+}
+
+/*
+ * add sku to the list of attributes to be ignored when duplicating..
+ * this approach doesn't work because sku is explicitly set by product_duplicate
+ * and is not treated as meta and not deleted from the meta of the cloned product
+  function ink_duplicate_product_exclude_meta( $excludes ) {
+  $excludes[] = '_sku';
+  $excludes[] = 'sku';
+  return $excludes;
+  }
+ */
 
 /*
  * On save of product, if there is no sku set, set the sku
@@ -98,17 +118,16 @@ function ink_duplicate_product_exclude_meta( $excludes ) {
  *
  * @return boolean false if something went wrong
  */
-/*
-  function ink_maybe_generate_variation_sku( $product, $i ) {
-  // Checks to ensure we have a product and gets one if we have an ID
-  if ( is_numeric( $product ) ) {
-  $product = wc_get_product( absint( $product ) );
-  }
-  //$variationslug = ink_get_formatted_variation_slug( $product );
-  $parent = $product->get_parent();
-  maybe_save_variation_sku( $product, $parent );
-  }
- */
+function ink_maybe_generate_variation_sku( $product, $i ) {
+	// Checks to ensure we have a product and gets one if we have an ID
+	if ( is_numeric( $product ) ) {
+		$product = wc_get_product( absint( $product ) );
+	}
+	//$variationslug = ink_get_formatted_variation_slug( $product );
+	$parent = $product->get_parent_id();
+	maybe_save_variation_sku( $product, $parent );
+}
+
 /**
  * if it's a valid product with no sku,
  *  generate sku and update the product
@@ -119,6 +138,9 @@ function ink_duplicate_product_exclude_meta( $excludes ) {
 function ink_maybe_generate_and_save_sku( $product ) {
 
 	// Checks to ensure we have a product and gets one if we have an ID
+	if ( ! $product ) {
+		return;
+	}
 	if ( is_numeric( $product ) ) {
 		$product = wc_get_product( absint( $product ) );
 	}
@@ -126,32 +148,30 @@ function ink_maybe_generate_and_save_sku( $product ) {
 	if ( ! $product->get_id() && strpos( skuformat(), '{id}' ) ) {
 		return '';
 	}
-
-	$current_sku = $product->get_sku();
-	if ( $current_sku && $current_sku != '' ) {
-		return $current_sku;
-	}
-
+	//no generation for translations,
+	//will be generated in base language and copied to translation
 	if ( post_is_translation( $product->get_id() ) ) {
 		return '';
 	}
 
-	// Generate the SKU for simple / external / variable parent products
-	$product_sku = generate_product_sku( $product );
+	$current_sku = $product->get_sku();
+	if ( ! $current_sku || $current_sku == '' ) {
+		// Generate the SKU for simple / external / variable parent products
+		$current_sku = generate_product_sku( $product );
+		// save the SKU for simple / external / parent products if we should
+		$product->set_sku( $current_sku );
+		$product->save();
+	}
 
 	// Only generate / save variation SKUs when we should
 	if ( $product->is_type( 'variable' ) ) {
 		$variations = $product->get_children();
 		foreach ( $variations as $variation_id ) {
-			maybe_save_variation_sku( $variation_id, $product, $product_sku );
+			maybe_save_variation_sku( $variation_id, $product, $current_sku );
 		}
 	}
 
-	// save the SKU for simple / external / parent products if we should
-	$product->set_sku( $product_sku );
-	$product->save();
-
-	return $product_sku;
+	return $current_sku;
 }
 
 /*
@@ -160,13 +180,19 @@ function ink_maybe_generate_and_save_sku( $product ) {
  * @param \WC_Product|int $product WC_Product object or product ID
  */
 function generate_product_sku( $product ) {
+	if ( ! $product ) {
+		return;
+	}
+	$product = wc_get_product( $product );
+
 	//if format includes initials, get initials from product title and substitute them
 	$newsku = skuformat();
 	if ( strpos( $newsku, '{initials}' ) ) {
 		$initials	 = ink_initials( $product->get_title() );
 		$newsku		 = str_replace( '{initials}', $initials, $newsku );
 	}
-	$newsku = str_replace( '{id}', $product->get_id(), $newsku );
+	$newsku	 = str_replace( '{id}', $product->get_id(), $newsku );
+	$newsku	 = ensure_unique_sku( $product->get_id(), $newsku );
 	return $newsku;
 }
 
@@ -178,24 +204,33 @@ function generate_product_sku( $product ) {
  * @param string $parent_sku if being generated, the new parent SKU
  */
 function maybe_save_variation_sku( $variation_id, $parent, $parent_sku = null ) {
+	$parent = wc_get_product( $parent );
+	if ( ! $parent ) {
+		return;
+	}
+
 	if ( post_is_translation( $parent->get_id() ) ) {
 		return;
 	}
-	$variation	 = wc_get_product( $variation_id );
-	$parent_sku	 = $parent_sku ? $parent_sku : $parent->get_sku();
-	if ( $variation->is_type( 'variation' ) && ! empty( $parent_sku ) ) {
-		$variation_slug	 = ink_get_formatted_variation_slug( $variation, true );
-		$oldsku			 = $variation->get_sku();
+	$variation = wc_get_product( $variation_id );
+	if ( ! $variation ) {
+		return;
+	}
+	$parent_sku = $parent_sku ? $parent_sku : $parent->get_sku();
+	if ( $variation->is_type( 'variation' ) && ( ! empty( $parent_sku ) ) ) {
+		$variation_slug = ink_get_formatted_variation_slug( $variation, true );
 		if ( $variation_slug != '' ) {
-			$variation->set_name( $parent->get_name() . '-' . $variation_slug );
+			$newname = $parent->get_name() . '-' . $variation_slug;
+			$variation->set_name( $newname );
 		}
-		if ( $oldsku == '' ) {
+		$oldsku = $variation->get_sku();
+		if ( $oldsku == '' || $oldsku == $parent_sku ) {
 			if ( $parent_sku == '' ) {
 				$parent_sku = ink_maybe_generate_and_save_sku( $parent );
 			}
 			$variation_slug = ink_get_formatted_variation_slug( $variation, false );
 			if ( $variation_slug && $parent_sku ) {
-				$sku = $parent_sku . '-' . $variation_slug;
+				$sku = ensure_unique_sku( $variation->get_id(), $parent_sku . '-' . $variation_slug );
 				$variation->set_sku( $sku );
 				$variation->save();
 			}
@@ -211,6 +246,10 @@ function maybe_save_variation_sku( $variation_id, $parent, $parent_sku = null ) 
 function ajax_maybe_generate_variation_skus( $parent_id ) {
 
 	$product = wc_get_product( $parent_id );
+	if ( ! $product ) {
+		return;
+	}
+
 
 	if ( $product instanceof WC_Product && $product->is_type( 'variable' ) && 'never' !== get_option( 'wc_sku_generator_variation' ) ) {
 
@@ -262,6 +301,10 @@ function ink_initials( $input ) {
  * @return string
  */
 function ink_get_formatted_variation_slug( $variation, $is_name ) {
+	if ( ! $variation ) {
+		return '';
+	}
+
 	$return = '';
 
 	if ( is_a( $variation, 'WC_Product_Variation' ) ) {
@@ -298,11 +341,42 @@ function ink_get_formatted_variation_slug( $variation, $is_name ) {
 			if ( $is_name && ('' === $value || ( wc_is_attribute_in_product_name( $value, $variation_name ) ) ) ) {
 				continue;
 			}
-
-			$variation_list[] = rawurldecode( $value );
+			$attrslug = rawurldecode( $value );
+			if ( ! $is_name ) {
+				$attrslug	 = preg_replace( '/[^A-Za-z0-9\-]/', '', $attrslug );
+				$attrslug	 = str_replace( '-', '', $attrslug );
+				if ( strlen( $attrslug ) > 4 ) {
+					$attrslug = strtoupper( substr( $attrslug, 0, 4 ) );
+				}
+			}
+			$variation_list[] = $attrslug;
 		}
-
-		$return .= implode( ', ', $variation_list );
+		if ( $is_name ) {
+			$return .= implode( ', ', $variation_list );
+		} else {
+			$return .= implode( '-', $variation_list );
+		}
 	}
 	return $return;
+}
+
+/*
+ * ensure a unique sku is generated:
+ * if it is not unique, add letters to the end until it is.
+ * (should never happen with products if id included, could happen with variations)
+ * Note woocommerce does have a wc_product_generate_unique_sku() function
+ * which adds numbers to the end..  though as the default sku here
+ *
+ * @param int    $product_id Product ID.
+ * @param string $sku Product SKU.
+ * @return string validated unique sku
+ */
+function ensure_unique_sku( $product_id, $sku ) {
+	if ( wc_product_has_unique_sku( $product_id, $sku ) ) {
+		return $sku;
+	} else {
+		//random upper case letter to be added to the end
+		$letter = chr( rand( 65, 90 ) );
+		return ensure_unique_sku( $product_id, $sku . $letter );
+	}
 }
