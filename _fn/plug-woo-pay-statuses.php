7df4fed9c302145859c7d29b07398e4050a57019
Jonathan Moore <Jonathan.Moore@bcs.org>
2019-02-14 21:17:50 +0000

    remove pay options from shipped status

--- _fn/plug-woo-pay-statuses.php
+++ _fn/plug-woo-pay-statuses.php
@@ -16,11 +16,22 @@
  * @return Array                array of valid order status strings
  */
 function inkston_allow_pay_onhold( $valid_order_statuses, $order ) {
-    $valid_order_statuses[] = 'on-hold';
-    return $valid_order_statuses;
+	$valid_order_statuses[] = 'on-hold';
+	return $valid_order_statuses;
 }
 
 add_filter( 'woocommerce_valid_order_statuses_for_payment', 'inkston_allow_pay_onhold', 10, 2 );
+
+/*
+ * custom statuses for shipped are not perceived as paid by woocommerce due to this filter
+ */
+function inkston_shipped_is_paid( $paid_order_statuses ) {
+	$paid_order_statuses[] = 'shipped';
+	return $paid_order_statuses;
+}
+
+//apply_filters( 'woocommerce_order_is_paid_statuses', array( 'processing', 'completed' ) );
+add_filter( 'woocommerce_order_is_paid_statuses', 'inkston_shipped_is_paid', 10, 1 );
 
 
 /*
@@ -31,8 +42,8 @@ add_filter( 'woocommerce_valid_order_statuses_for_payment', 'inkston_allow_pay_o
  * @return Array                array of valid order status strings
  */
 function inkston_allow_cancel_onhold( $valid_order_statuses ) {
-    $valid_order_statuses[] = 'on-hold';
-    return $valid_order_statuses;
+	$valid_order_statuses[] = 'on-hold';
+	return $valid_order_statuses;
 }
 
 add_filter( 'woocommerce_valid_order_statuses_for_cancel', 'inkston_allow_cancel_onhold', 10, 1 );
@@ -55,8 +66,10 @@ function ii_show_order_payment_link( $order ) {
  * ... it's crazy that you have to exit the order details screen and go back to the order list to be able to pay...
  */
 function ii_show_pay_button( $order ) {
-	echo ('<a href="' . esc_url( $order->get_checkout_payment_url() ) . '" class="woocommerce-button button pay">' .
-	esc_html( __( 'Pay', 'woocommerce' ) ) . '</a>');
+	if ( ! $order->is_paid() ) {
+		echo ('<a href="' . esc_url( $order->get_checkout_payment_url() ) . '" class="woocommerce-button button pay">' .
+		esc_html( __( 'Pay', 'woocommerce' ) ) . '</a>');
+	}
 }
 
 add_action( 'woocommerce_order_details_before_order_table', 'ii_show_order_payment_link', 10, 1 );
